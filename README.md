c2dffd29d2899aad80363c671d8f35ec33d123c1
likun <496546144@qq.com>
2019-04-24 11:03:36 +0800

    优化

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
-[![Release](https://jitpack.io/v/ashLikun/XViewPager.svg)](https://jitpack.io/#ashLikun/XViewPager)
+[![Release](https://jitpack.io/v/ashLikun/GlideUtils.svg)](https://jitpack.io/#ashLikun/GlideUtils)
 
-# **XViewPager**
+# **GlideUtils**
 
 ## 使用方法
 
@@ -17,62 +17,15 @@ allprojects {
 
 ```gradle
 dependencies {
-    implementation 'com.github.ashLikun:XViewPager:{latest version}'//XViewPager
+    implementation 'com.github.ashLikun:GlideUtils:{latest version}'//GlideUtils
 }
 ```
 ### 1.用法
 ```java
-         <com.ashlikun.xviewpager.ConvenientBanner
-                android:id="@+id/convenientBanner"
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:layout_marginTop="10dp"
-                app:banner_turningTime="3000"
-                app:banner_isOneDataOffLoopAndTurning="false"
-                app:banner_isCanTouchScroll="true"
-                app:banner_ratio="2" />
-
-         <!--是否可以循环 默认true-->
-            <attr name="banner_canLoop" format="boolean" />
-            <!--自动播放时间间隔 默认5000-->
-            <attr name="banner_turningTime" format="integer" />
-            <!--是否可以触摸滚动, 默认true-->
-            <attr name="banner_isCanTouchScroll" format="boolean" />
-            <!--是否可以自动定时翻页-->
-            <attr name="banner_isAutoTurning" format="boolean" />
-            <!--是否只有一条数据的时候禁用翻页, 默认true-->
-            <attr name="banner_isOneDataOffLoopAndTurning" format="boolean" />
-        
-            <!--Indicatorde 属性-->
-            <!--间距-->
-            <attr name="ind_space" format="dimension" />
-            <!--未选中的资源Id-->
-            <attr name="ind_no_select" format="reference" />
-            <!--选中的资源Id-->
-            <attr name="ind_select" format="reference" />
-            <!--比例   默认16/9.0f-->
-            <attr name="banner_ratio" format="reference|float"></attr>
-            <!--参考方向-->
-            <attr name="banner_orientation">
-                <enum name="width" value="0" />
-                <enum name="height" value="1" />
-            </attr>
-
-        convenientBanner.setIndicator(new ZoomIndicator(this));
-        bannerViewPager.setPages(this, new ArrayList(Arrays.asList(RESURL)));
-        convenientBanner.setPages(this, new ArrayList(Arrays.asList(RESURL)));
-
-        //创建itemview由使用者实现
-        @Override
-        public View createView(Context context, BannerViewPager banner, String data) {
-            ImageView imageView = new ImageView(context);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(-1, -1));
-            GlideUtils.show(imageView, data);
-            return imageView;
-        }
+       
 ```
 ### 混肴
+glide混淆
 ####
 
 
--- app/src/main/java/com/ashlikun/xviewpager/simple/MainActivity.java
+++ app/src/main/java/com/ashlikun/xviewpager/simple/MainActivity.java
@@ -1,28 +1,12 @@
 package com.ashlikun.xviewpager.simple;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.ashlikun.glideutils.GlideUtils;
-import com.ashlikun.xviewpager.ConvenientBanner;
-import com.ashlikun.xviewpager.indicator.TransIndicator;
-import com.ashlikun.xviewpager.listener.OnItemClickListener;
-import com.ashlikun.xviewpager.listener.ViewPageHelperListener;
-import com.ashlikun.xviewpager.view.BannerViewPager;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class MainActivity extends AppCompatActivity implements ViewPageHelperListener<String> {
-    BannerViewPager bannerViewPager;
-    ConvenientBanner convenientBanner;
+public class MainActivity extends AppCompatActivity {
     private static final Object[] RESURL = {
             "http://img.mukewang.com/54bf7e1f000109c506000338-590-330.jpg",
             "http://upload.techweb.com.cn/2015/0114/1421211858103.jpg",
@@ -36,45 +20,9 @@ public class MainActivity extends AppCompatActivity implements ViewPageHelperLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bannerViewPager = findViewById(R.id.bannerViewPager);
-//        bannerViewPager.setRefreshLayout(findViewById(R.id.swipeRefresh));
-        convenientBanner = findViewById(R.id.convenientBanner);
-        convenientBanner.setIndicator(new TransIndicator(this));
-        bannerViewPager.setPages(this, new ArrayList(Arrays.asList(RESURL)));
-        convenientBanner.setPages(this, new ArrayList(Arrays.asList(RESURL2)));
-        convenientBanner.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(BannerViewPager banner, Object data, int position) {
-                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_LONG).show();
-            }
-        });
+        ImageView imageView = findViewById(R.id.imageView);
+        GlideUtils.show(imageView, RESURL[0]);
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
-//        if (bannerViewPager.getRealItemCount() == RESURL.length) {
-//            bannerViewPager.setPages(new ArrayList(Arrays.asList(RESURL2)));
-//            convenientBanner.setPages(new ArrayList(Arrays.asList(RESURL2)));
-//        } else {
-//            bannerViewPager.setPages(new ArrayList(Arrays.asList(RESURL)));
-//            convenientBanner.setPages(new ArrayList(Arrays.asList(RESURL)));
-//        }
-    }
-
-    @Override
-    public View createView(Context context, BannerViewPager banner, String data, int position) {
-        ImageView imageView = new ImageView(context);
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        imageView.setLayoutParams(new ViewGroup.LayoutParams(-1, -1));
-        GlideUtils.show(imageView, data);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "aaaaa", Toast.LENGTH_LONG).show();
-            }
-        });
-        return imageView;
-    }
 
 }
